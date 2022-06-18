caee6961eff757d9a6ef79aee25749d66597f095
samckp <sam.mca.liet@gmail.com>
2022-06-18 21:10:24 +0530

    code updated

--- src/main/java/com/test/java8/CountCharsInString.java
+++ src/main/java/com/test/java8/CountCharsInString.java
@@ -1,13 +1,33 @@
 package com.test.java8;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CountCharsInString {
 
     public static void main(String[] args) {
 
         String str = "this is a string";
+
+        charOccurenceCount(str);
+
+        charOccurenceCountUsingStream(str);
+    }
+
+    private static void charOccurenceCountUsingStream(String str){
+
+        Map<String, Long> result = Arrays.stream(str.split(""))
+                .collect(
+                        Collectors.groupingBy(Function.identity(), Collectors.counting())
+                );
+        System.out.println(result);
+    }
+
+
+    private static void charOccurenceCount(String str) {
 
         char []chars = str.toCharArray();
         Integer count =0;
@@ -27,4 +47,5 @@ public class CountCharsInString {
             System.out.println(entry.getKey() +" : " + entry.getValue());
         }
     }
+
 }
--- src/main/java/com/test/java8/Test.java
+++ src/main/java/com/test/java8/Test.java
@@ -1,6 +1,7 @@
 package com.test.java8;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,26 +10,26 @@ public class Test {
 
     public static void main(String[] args) {
 
-//    public void testName(){
         List<Employee> emps = new ArrayList<>();
 
         emps.add(new Employee(1,"Sam",23,"male","IT",2022,3333));
-        emps.add(new Employee(2,"SaP",23,"female","QA",2021,2112));
-        emps.add(new Employee(3,"Yam",23,"male","DA",2011,34234));
-        emps.add(new Employee(4,"tam",23,"female","IT",2011,1111));
-
+        emps.add(new Employee(2,"SaP",22,"female","QA",2021,2112));
+        emps.add(new Employee(3,"Yam",66,"male","DA",2011,34234));
+        emps.add(new Employee(4,"tam",53,"female","IT",2011,1111));
 
         List<Employee> nameS = emps.stream().filter(e->e.getName().startsWith("S")).collect(Collectors.toList());
 
         System.out.println(nameS);
 
-
         Map<String, Double> avgSalByDEpt =
-                emps.stream().collect(Collectors.
-                        groupingBy(Employee::getDepartment,
-                                Collectors.averagingDouble(Employee::getSalary)));
-
+                                emps.stream().collect(Collectors.
+                                        groupingBy(Employee::getDepartment,
+                                                Collectors.averagingDouble(Employee::getSalary)));
         System.out.println(avgSalByDEpt);
+
+
+        Comparator<Employee> sortByAge =  Comparator.comparing(Employee::getAge);
+        emps.stream().sorted(sortByAge).forEach(System.out::println);
 
     }
 }
